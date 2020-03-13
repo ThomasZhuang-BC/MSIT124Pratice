aa0bb1acfa8cecc4151771851af92ce341bc1eaa
JDW-Daniel <jiandewux1123@gmail.com>
2020-03-13 13:56:53 +0800

    我新增一個BTN

---
+++ MSIT124Practice/MSIT124Practice/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSIT124Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("公子面試70%女性的公司都是大媽");
+        }
+    }
+}
